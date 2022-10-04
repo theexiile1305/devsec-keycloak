808a278c3e533f8b4c000ceaa5ea155278a2e866
Clemens Hübner <clemens.huebner@inovex.de>
2022-10-04 17:26:17 +0200

    add private area

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,20 +18,39 @@ function App({keycloak}) {
                 values: ["private"]
             }
         })
-            .then(res => {
-                window.location.reload();
-            });
+        localStorage.setItem("private", "true")
     };
+
+    const home = () => {
+        console.log("Home")
+        localStorage.removeItem("private")
+        window.location.reload();
+    };
+
+    const content = () => {
+        console.log(localStorage.getItem("private"))
+        if(localStorage.getItem("private")){
+            return <div>
+                <h1>Private Area</h1>
+                <p>See secret stuff here...</p>
+            </div>
+        } else {
+            return <NoteList />
+        }
+    }
 
     return (
         <div className="note-app">
+            <div className="note-button" onClick={home}>
+                Home
+            </div>
             <div className="note-button" onClick={stepUp}>
                 Private Area
             </div>
             <div className="note-button logout" onClick={logout}>
                 Logout
-            </div>Okay
-            <NoteList />
+            </div>
+            { content() }
         </div>
     );
 }
